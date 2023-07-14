98c00dd7e5b6d90a81f3348b7fa1f71799f3d57e
Indrajit Pal <palindrajit10@gmail.com>
2023-07-14 16:15:01 +0530

    updated backend

--- src/components/BackendStatus.jsx
+++ src/components/BackendStatus.jsx
@@ -5,7 +5,7 @@ function BackendStatus() {
 
     useEffect(() => {
         const local_func = async () => {
-            const response = await fetch("react-chat-app-server-gray.vercel.app");
+            const response = await fetch("https://chat-app-backend-snowy.vercel.app");
             if (response.status === 200) {
                 setIsBackendUp(true);
             }
